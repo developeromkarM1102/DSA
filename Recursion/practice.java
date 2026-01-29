416fc47e9b05885f2dda615c1c27d0c8de8ebd52
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-01-29 17:55:53 +0530

    solving an recursion q

---
+++ Recursion/practice.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+import java.util.Scanner;
+
+//understanding the concept of recursion
+//print "Hello" n times using recursion
+public class practice {
+
+    void function(int n){
+        if(n==0)
+        {
+            return;
+        }
+
+        System.out.println("Hello");
+        function(n-1);
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=sc.nextInt();
+
+        practice obj=new practice();
+        obj.function(n);
+    }
+}
