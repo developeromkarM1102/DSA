2c366032a22c0091464f700ca2efb779497e90db
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-02-04 19:14:10 +0530

    solving an recursion q

---
+++ Recursion/String_palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        return check(s, 0, s.length() - 1);
+    }
+
+    private boolean check(String s, int left, int right) {
+
+        while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+            left++;
+        }
+
+        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            right--;
+        }
+
+        if (left >= right) return true;
+
+        if (Character.toLowerCase(s.charAt(left)) !=
+            Character.toLowerCase(s.charAt(right))) {
+            return false;
+        }
+
+        return check(s, left + 1, right - 1);
+    }
+}
