3b632142c85627b3d4317c823b646c6174ee9d24
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-03-02 16:56:52 +0530

    new

---
+++ Basics/BitwiseOperator.java
@@ -0,0 +1,21 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a =6; 
+        int b =10;
+
+        //Bitwise AND
+        System.out.println("\n"+(a&b));
+
+        //Bitwise OR
+        System.out.println("\n"+(a|b));
+
+        //Bitwise XOR
+        System.out.println("\n"+(a^b));
+
+        //Bitwise left shift operator
+        System.out.println("\n"+(10<<2));
+
+        //Bitwise right shift operator
+        System.out.println("\n"+(10>>1));
+    }
+}
