90bbbdb7a1515f99e1710301f766b8a194003880
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-03-06 16:07:21 +0530

    vectors

--- Arrays/Vectors.java
+++ Arrays/Vectors.java
@@ -2,15 +2,20 @@ import java.util.Vector;
 public class Vectors {
     public static void main(String[] args) {
         Vector<Integer> v = new Vector<Integer>();
+        //it is like an array but arrays are statics so we cannot add elements in that 
+        //vectors are dynamics in nature where we can add and delete the elements 
 
-        v.add(10);
-        v.add(20);
-        v.add(30);
-
-        System.out.println(v);
-
-        v.remove(1);
+        v.add(10); //to add elements in vectors
+        v.add(20); //to add elements in vectors
+        v.add(30); //to add elements in vectors
 
         System.out.println(v);
+
+        v.remove(1); // to remove the element
+        v.add(40);
+
+        System.out.println(v.firstElement());//to get an first element in vectors
+        System.out.println(v.lastElement());//to get an last element in vector
+    
     }
 }
