4d7014d416a45af198325b65ca5da8719d438731
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-03-06 15:55:21 +0530

    vector

---
+++ Arrays/Vectors.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+public class Vectors {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<Integer>();
+
+        v.add(10);
+        v.add(20);
+        v.add(30);
+
+        System.out.println(v);
+
+        v.remove(1);
+
+        System.out.println(v);
+    }
+}
