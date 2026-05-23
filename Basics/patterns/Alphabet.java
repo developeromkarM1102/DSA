3460bfb8409fa9ab3cc07e0f20792eeca3535647
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-05-23 16:39:49 +0530

    new q

---
+++ Basics/patterns/Alphabet.java
@@ -0,0 +1,21 @@
+
+public class Alphabet {
+    public static void main(String[] args) {
+        int n = 5; // You can change this value to adjust the pattern size
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+        /* This is the pattern that will be printed for n=5:
+         * A
+         * A B
+         * A B C
+         * A B C D
+         * A B C D E
+         */
+    }
+}
