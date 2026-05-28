9bb01ffa1a0130ee82387d1af0b6d4b66eeeebe8
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-05-28 17:07:42 +0530

    new

---
+++ Arrays/leftRotateByK.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class leftRotateByK {
+    public static void main(String[] args) {
+        // Code here
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int k = sc.nextInt();
+
+        int arr [] = new int [n];
+
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();   
+        }
+
+        for(int j=1;j<=k;j++){
+
+        int temp = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1] = temp;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " " );
+        }
+    }
+}
