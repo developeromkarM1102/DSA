42647c09657fe6a5725353f87b999dfaaff07465
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-05-30 20:07:21 +0530

    Fixed bubble sort logic

--- Basics/Sorting/bubble_sort.java
+++ Basics/Sorting/bubble_sort.java
@@ -4,29 +4,38 @@ import java.util.Scanner;
 
 public class bubble_sort {
 
-    public static void bubbleSort(int arr[] , int n){
-        for(int i=n-1;i>=1;i--){
-            for(int j=0;j<i;j++){
-                if(arr[j]>arr[j+1]){
+    public static void bubbleSort(int arr[], int n) {
+
+        //logic is to compare the adjacent elements and swap them if they are in the wrong order
+
+        //optimised approach soln
+        boolean isSwap = false;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSwap = true;
                 }
             }
+            if (!isSwap) {
+                break;
+            }
         }
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
-    
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int arr[] = new int[n];
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         bubbleSort(arr, n);
