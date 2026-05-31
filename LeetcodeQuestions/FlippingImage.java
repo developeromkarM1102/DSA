272635229e2be138f56026f5d5dd76051d5d589d
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-05-31 10:35:17 +0530

    new LQ

---
+++ LeetcodeQuestions/FlippingImage.java
@@ -0,0 +1,32 @@
+package LeetcodeQuestions;
+
+public class FlippingImage {
+
+    public int[][] flipAndInvertImage(int[][] image) {
+        
+        int n = image.length;
+
+        //reverse || flip the 2D Array
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = image[i][j];
+                image[i][j] = image[i][n-1-j];
+                image[i][n-1-j] = temp;
+            }
+        }
+
+        //inverting the 2D Array
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(image[i][j]==1)
+                    image[i][j]=0;
+                else
+                    image[i][j]=1;
+            }
+        }
+
+        return image;
+    }
+}
