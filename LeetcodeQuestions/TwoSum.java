fabb1ffc6ec34880026daa798cf279de780f44a5
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-06-02 15:18:25 +0530

    new LQ

---
+++ LeetcodeQuestions/TwoSum.java
@@ -0,0 +1,23 @@
+package LeetcodeQuestions;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        //Brute force soln
+        
+        int ans[]= new int[2];
+        int n = nums.length;
+    
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i] + nums[j] == target){
+                   ans[0]=i;
+                   ans[1]=j;
+                   break;
+                }
+            }
+        }
+        return ans;
+    }
+}
