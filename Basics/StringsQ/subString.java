d7328c74c9228106be08d7f02dbba75d146e35e8
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-06-18 21:16:45 +0530

    adding Substring Q

---
+++ Basics/StringsQ/subString.java
@@ -0,0 +1,28 @@
+package StringsQ;
+
+public class subString {
+
+    int firstOccurence(String txt, String pat) {
+		// code here
+		int n = txt.length();
+		int m = pat.length();
+		
+		for (int i = 0; i <= n - m; i++) {
+			
+			int j;
+			for (j = 0; j < m; j++) {
+				
+				if (txt.charAt(i + j) != pat.charAt(j)) {
+					break;
+				}
+			}
+			
+			if (j == m) {
+				
+				return i;
+			}
+		}
+		
+		return - 1;
+	}
+}
