da0ed2dbf09f82a66bc7747c73e4dd92f52fcd40
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-06-24 20:04:28 +0530

    Compare two approaches for Kadane's Algorithm

--- LeetcodeQuestions/KadanesAlgo.java
+++ LeetcodeQuestions/KadanesAlgo.java
@@ -3,6 +3,10 @@ package LeetcodeQuestions;
 public class KadanesAlgo {
 
     //Kadane's Algorithm || Maximum Subarray Sum
+    // Should I:
+    // 1. Start a new subarray from nums[i]
+    // OR
+    // 2. Continue the previous subarray?
     
     public int maxSubArray(int[] nums) {
         int cs = nums[0];
@@ -12,6 +16,31 @@ public class KadanesAlgo {
             cs = Math.max(nums[i], cs + nums[i]);
             maxSum = Math.max(maxSum, cs);
         }
+        return maxSum;
+    }
+}
+
+//Another way of thinking to solve, 
+// If current sum becomes negative,throw it away and start fresh. 
+
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+
+        int n = nums.length;
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            
+            sum += nums[i];
+            maxSum = Math.max(maxSum , sum);
+
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        
         return maxSum;
     }
 }
