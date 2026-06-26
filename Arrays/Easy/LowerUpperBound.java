d5bd404eb4ba04db0f167277fe69a7dd14596f65
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-06-26 18:07:44 +0530

    finding lower bound using binary search

---
+++ Arrays/Easy/LowerUpperBound.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class LowerUpperBound {
+    
+    public static int getLowerBound(int arr[] , int target){
+
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+
+        while (start <= end) {
+
+            int mid = start + (end-start) /2;
+
+            if(arr[mid]>=target){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {10,10,30,30,30,30,30,40,50};
+        int target = 30;
+        int ans = getLowerBound(arr, target);
+        System.out.println("ans : " + ans);
+    }
+}
