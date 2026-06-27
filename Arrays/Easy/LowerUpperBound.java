d405f24d14a586fe562f69aee2008f00e2d8c849
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-06-27 11:57:03 +0530

    finding upper bound using binary search

--- Arrays/Easy/LowerUpperBound.java
+++ Arrays/Easy/LowerUpperBound.java
@@ -23,11 +23,33 @@ public class LowerUpperBound {
         }
         return ans;
     }
+    public static int getUpperBound(int arr[] , int target){
+
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+
+        while (start <= end) {
+
+            int mid = start + (end-start) /2;
+
+            if(arr[mid]<=target){
+                start = mid + 1;
+            }
+            else{
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         int arr [] = {10,10,30,30,30,30,30,40,50};
         int target = 30;
-        int ans = getLowerBound(arr, target);
+        //int ans = getLowerBound(arr, target);
+        int ans = getUpperBound(arr,target);
         System.out.println("ans : " + ans);
     }
 }
