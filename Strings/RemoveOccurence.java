20ce9e0f1e37cf7d4ffcfe1eea3278132a5da224
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-07-12 16:24:46 +0530

    Solved RemoveOccurence

---
+++ Strings/RemoveOccurence.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class RemoveOccurence {
+
+    public String removeOccurrences(String s, String part) {
+
+        while (s.contains(part)) {
+
+            int index = s.indexOf(part);
+
+            s = s.substring(0, index) + s.substring(index + part.length());
+        }
+        return s;
+    }
+}
