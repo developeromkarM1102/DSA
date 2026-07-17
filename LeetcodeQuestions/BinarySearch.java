db5053c0cd460b4702914a10ae7ddf24d00f22c0
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-07-17 20:42:07 +0530

    Solved BS using Recursion

--- LeetcodeQuestions/BinarySearch.java
+++ LeetcodeQuestions/BinarySearch.java
@@ -1,34 +1,62 @@
 package LeetcodeQuestions;
 
 public class BinarySearch {
-    
+
     public int search(int[] nums, int target) {
-        
+
         int ans = -1;
         int n = nums.length;
-        int s=0;
-        int e=n-1;
+        int s = 0;
+        int e = n - 1;
 
-        while(s<=e){
+        while (s <= e) {
 
-            //int mid = (s+e)/2;
+            // int mid = (s+e)/2;
 
-            //using following mid to avoid constraint overflowing
+            // using following mid to avoid constraint overflowing
 
             int mid = s + (e - s) / 2;
-            
-            if(nums[mid]==target){
+
+            if (nums[mid] == target) {
                 ans = mid;
                 break;
-            }
-            else if(nums[mid]<target){
+            } else if (nums[mid] < target) {
                 s = mid + 1;
-            }
-            else{
+            } else {
                 e = mid - 1;
             }
 
         }
-            return ans;
+        return ans;
+    }
+
+    // using Recursion
+    public int solve(int[] nums, int target, int s, int e) {
+
+        // base case
+        if (s > e) {
+            return -1;
+        }
+
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            e = mid - 1;
+        } else {
+            s = mid + 1;
+        }
+
+        return solve(nums, target, s, e);
+
+    }
+
+    public int search(int[] nums, int target) {
+
+        int s = 0;
+        int e = nums.length - 1;
+        int ans = solve(nums, target, s, e);
+        return ans;
     }
 }
