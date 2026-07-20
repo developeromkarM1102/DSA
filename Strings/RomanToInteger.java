8a39ee32acbca1348f3000d405b61e61b65d496a
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-07-20 19:48:52 +0530

    Solved RomanToInteger Q

---
+++ Strings/RomanToInteger.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i <= s.length() - 1; i++) {
+
+            int current = map.get(s.charAt(i));
+
+            if (i < s.length() - 1 && current < map.get(s.charAt(i + 1))) {
+                ans -= current;
+            } else {
+                ans += current;
+            }
+        }
+        return ans;
+    }
+}
