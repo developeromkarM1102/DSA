9fbfb7734f78f4efc999116e001c966e45e01dbf
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-07-26 17:40:08 +0530

    Solved SubSet1 & SubSetDup/2 with include-exclude pattern using recursion

--- LeetcodeQuestions/SubSet2.java
+++ LeetcodeQuestions/SubSet2.java
@@ -1,10 +1,13 @@
+
 package LeetcodeQuestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class SubSet1 {
-        static void solve(int[] nums, int index, List<Integer> output, List<List<Integer>> ans) {
+public class SubSet2 {
+
+    static void solve(int[] nums, int index, List<Integer> output, List<List<Integer>> ans) {
 
         if (index >= nums.length) {
             ans.add(new ArrayList<>(output));
@@ -13,21 +16,26 @@ public class SubSet1 {
 
         int currentValue = nums[index];
 
-        //rescursion using include- exclude pattern
-        //include call
+        // rescursion using include- exclude pattern
+        // include call
         output.add(currentValue);
         solve(nums, index + 1, output, ans);
-
-        //backtraking step -> for return null value while coming back 
+        
+        // backtraking step -> for return null value while coming back
         output.remove(output.size() - 1);
 
-        //exclude call
+        // while excluding , ignore the same element and move towards the new element
+        while (index + 1 < nums.length && nums[index] == nums[index + 1]) {
+            index++;
+        }
+        // exclude call
         solve(nums, index + 1, output, ans);
 
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-
+        // bcz array is sorted
+        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         int index = 0;
@@ -35,3 +43,5 @@ public class SubSet1 {
         return ans;
     }
 }
+
+
