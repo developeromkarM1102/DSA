a0408c636ef56d6051e22bd8801fe23522f25d2c
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-08-12 13:07:46 +0530

    Solved LinkedList Cycle

---
+++ LeetcodeQuestions/LLCycle.java
@@ -0,0 +1,21 @@
+package LeetcodeQuestions;
+
+public class LLCycle {
+
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
