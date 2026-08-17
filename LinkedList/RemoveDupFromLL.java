af3d82a765a66e282631e8ba47f72b12c1803b5f
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-08-17 17:41:19 +0530

    Solved RemoveDupFromLL using 2 pointers

---
+++ LinkedList/RemoveDupFromLL.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class RemoveDupFromLL {
+
+    Node removeDuplicates(Node head) {
+        // code here
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // using two pointer approach
+        Node prev = head;
+        Node curr = head.next;
+
+        while (curr != null) {
+
+            if (curr.data != prev.data) {
+                curr = curr.next;
+                prev = prev.next;
+            } else {
+                prev.next = curr.next;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
