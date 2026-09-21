cf6af6138c10447e643ca6d360457406f4819f53
developeromkarm1102 <omkarmutal1102@gmail.com>
2026-09-21 23:27:53 +0530

    Solved Convert Binary number to Integer in LL

---
+++ LeetcodeQuestions/ConvertBinaryNoToIntegerInLL.java
@@ -0,0 +1,14 @@
+package LeetcodeQuestions;
+
+public class ConvertBinaryNoToIntegerInLL {
+
+    public int getDecimalValue(ListNode head) {
+        
+        int ans = 0;
+        while(head != null){
+            ans = ans * 2 + head.val;
+            head = head.next;
+        }
+        return ans;
+    }
+}
